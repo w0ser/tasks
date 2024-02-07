fd75a70161621b889cd54c2992a523da1c1e2cfc
aa <65656053+w0ser@users.noreply.github.com>
2024-02-07 13:36:37 +0900

    Add files via upload

---
+++ task5/task3.js
@@ -0,0 +1,20 @@
+let answer1 = confirm('JavaScript появился в 1995 году?')
+if (answer1) {
+    alert('Верно')
+} else {
+    alert('Да')
+}
+
+let answer2 = confirm('Спецификация JavaScript называется ECMAScript?')
+if (answer2) {
+    alert('Верно')
+} else {
+    alert('ECMAScript — это стандарт ECMA-262, который называется «Спецификация языка ECMAScript')
+}
+
+let answer3 = confirm('JavaScript был создан за 1 месяц?')
+if (answer3) {
+    alert('Верно')
+} else {
+    alert('JavaScript был создан за 10 дней автором Бренданом Айком')
+}
